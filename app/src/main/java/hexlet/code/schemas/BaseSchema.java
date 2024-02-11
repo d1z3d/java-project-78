53a5b8e334a6926f944f4ed98dad5d39c83ef146
Borychev Andrey <borychev.au@gmail.com>
2024-02-11 10:18:22 +0300

    fix complexity

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,8 +5,9 @@ public abstract class BaseSchema<T> {
     protected boolean isRequired = false;
 
     public abstract boolean isValid(T object);
+
     public BaseSchema required() {
         this.isRequired = true;
         return this;
     }
-    }
+}
